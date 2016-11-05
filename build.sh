d7a0a3c32926fcdaec6d4dfa194e53fe29044684
darealshinji <djcj@gmx.de>
2016-11-06 00:19:03 +0100

    Make binary relocatable, fix screen selection, typo -> res[s]ources

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 LANG=C
 
+cflags="-Wall -O3 -fstack-protector -ffunction-sections -fdata-sections -DENABLE_BINRELOC -D_FORTIFY_SOURCE=2"
+
 set -v
 set -e
 
@@ -9,23 +11,25 @@ test -x libcxx/libc++-wrapper || ./build-libcxx.sh
 test -d fltk/build/usr/include || ./build-fltk.sh
 
 cd images
-xxd -i launcher.jpg > ../ressources.h
-xxd -i icon.png >> ../ressources.h
+xxd -i launcher.jpg > ../resources.h
+xxd -i icon.png >> ../resources.h
 cd ..
 
 fltk_version="$(echo `cat fltk/VERSION`)"
 fltk_revision="$(echo `svn info fltk | grep '^Revision: ' | awk '{print $2}'`)"
 
-libcxx/libc++-wrapper \
-  -std=c++11 -Wall -O3 \
-  -fstack-protector -ffunction-sections -fdata-sections -D_FORTIFY_SOURCE=2 \
+gcc $cflags -c -o binreloc.o binreloc.c
+
+libcxx/libc++-wrapper -std=c++11 $cflags \
   -DPRINT_VERSION=\"$fltk_version\" \
   -DPRINT_REVISION=\"$fltk_revision\" \
   -I./fltk \
   -I./fltk/build/usr/include \
   -I./fltk/build/usr/include/FL/images \
   -D_THREAD_SAFE -D_REENTRANT -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE \
-  -o launcher launcher.cpp \
+  -c -o launcher.o launcher.cpp
+
+libcxx/libc++-wrapper -o launcher launcher.o binreloc.o \
   -L./fltk/build/lib/ -lfltk_images -lfltk_jpeg -lfltk_png -lfltk_z -lfltk \
   -Wl,--as-needed -Wl,--gc-sections -Wl,-z,defs -Wl,-z,relro -s \
   -lm -lX11 -lXext -lpthread -lXinerama -lXfixes -lXcursor -lXft -lXrender -lfontconfig -ldl 
--- launcher.cpp
+++ launcher.cpp
@@ -44,6 +44,11 @@
 #include <string.h>
 #include <X11/Xlib.h>
 
+#ifndef ENABLE_BINRELOC
+#  define ENABLE_BINRELOC
+#endif
+#include "binreloc.h"
+
 #define STREQ(x, y) (strcmp(x, y) == 0)
 
 #define ITEM(x)    { x, 0, 0, 0, 0, FL_NORMAL_LABEL, 0, 14, 0 }
@@ -58,11 +63,12 @@
 #  endif
 #endif
 
-#ifndef EMBEDDED_RESSOURCES
-#  define EMBEDDED_RESSOURCES 1
+//#define EMBEDDED_RESOURCES 0
+#ifndef EMBEDDED_RESOURCES
+#  define EMBEDDED_RESOURCES 1
 #endif
-#if (EMBEDDED_RESSOURCES == 1)
-#  include "ressources.h"  /* the ressources are NOT MIT licensed */
+#if (EMBEDDED_RESOURCES == 1)
+#  include "resources.h"  /* the resources are NOT MIT licensed */
 #  define BGIMAGE   launcher_jpg
 #  define ICON      icon_png
 #  define ICON_LEN  icon_png_len
@@ -72,15 +78,9 @@
 
 
 static Fl_Window *win;
-static Fl_Choice *resolution_selection, *screen_selection, *language_selection;
-static Fl_Radio_Round_Button *highend, *lowend;
 static bool checkbutton_set;
 static bool launch_game = false;
-static int val_res, val_screen, screens_avail, is_fullscreen, val_lang, val_quality;
-
-const char *uri1 = "https://twitter.com/superhotthegame";
-const char *uri2 = "https://www.facebook.com/superhotgame";
-const char *uri3 = "mailto:people@superhotgame.com";
+static int val_quality;
 
 class uri_box : public Fl_Box
 {
@@ -153,12 +153,6 @@ static Fl_Menu_Item resolution_items[] = {
   ITEM_INIT
 };
 
-static Fl_Menu_Item monitor_items[128];
-
-static std::string l10n[] = {
-  "EN", "PL", "FR", "DE", "RU", "PT", "IT", "CZ", "HU", "SP"
-};
-
 static Fl_Menu_Item language_items[] = {
   ITEM("English"),
   ITEM("Polski"),
@@ -172,8 +166,6 @@ static Fl_Menu_Item language_items[] = {
   ITEM("Espa" "\xc3\xb1" "ol"),  /* Español */
   ITEM_INIT
 };
-
-static std::string quality[] = { "LowEnd", "HighEnd" };
 
 static std::string itostr(int i)
 {
@@ -254,39 +246,68 @@ static void rb_callback(Fl_Widget *, long p)
 
 int main(int argc, char **argv)
 {
+  Fl_Choice *resolution_selection, *screen_selection, *language_selection;
+  Fl_Menu_Item monitor_items[128];
+  std::string monitor_entry[128];
+
+  BrInitError brError;
+  char *exe, *exedir;
+  std::string aux, defPath, conffile;
+  int val_res, val_screen, screens_avail, is_fullscreen, val_lang;
+
   /* satisfying section 4 of the FLTK license's LGPL exception */
   std::cout << "using FLTK version " PRINT_VERSION " rev" PRINT_REVISION " (http://www.fltk.org)" << std::endl;
 
-  std::string aux(argv[0]);
-  int pos = aux.rfind('/');
-  std::string conffile = aux.substr(pos + 1) + ".config";
+  /* get exe full path */
+  if (!br_init(&brError))
+  {
+    std::cerr << "*** BinReloc failed to initialize. Error: ";
+    switch (brError)
+    {
+      case BR_INIT_ERROR_NOMEM:
+        std::cerr << "cannot allocate memory" << std::endl;
+      case BR_INIT_ERROR_OPEN_MAPS:
+        std::cerr << "unable to open /proc/self/maps" << std::endl;
+      case BR_INIT_ERROR_READ_MAPS:
+        std::cerr << "unable to read from /proc/self/maps" << std::endl;
+      case BR_INIT_ERROR_INVALID_MAPS:
+        std::cerr << "the file format of /proc/self/maps is invalid" << std::endl;
+      default:
+        std::cerr << brError << std::endl;
+    }
+  }
+  exe = br_find_exe(argv[0]);
+  aux = std::string(argv[0]);
+  int pos  = aux.rfind('/');
+  defPath  = aux.substr(0, pos + 1);
+  exedir   = br_find_exe_dir(defPath.c_str());
+  conffile = std::string(exe) + ".config";
 
-  val_res = readconf("resolution", conffile);
+  /* check configurations */
+  val_res       = readconf("resolution", conffile);
   is_fullscreen = readconf("fullscreen", conffile);
-  val_lang = readconf("language", conffile);
-  val_quality = readconf("quality", conffile);
-
+  val_lang      = readconf("language", conffile);
+  val_quality   = readconf("quality", conffile);
+  val_screen    = readconf("screen", conffile);
   screens_avail = get_number_of_screens();
-  val_screen = readconf("screen", conffile);
 
   if (val_screen > (screens_avail - 1) || val_screen < 0)
   {
     val_screen = 0;
   }
 
-  for (int i = 0; i <= screens_avail; ++i)
+  /* test screen selection */
+  //screens_avail = 5;
+
+  /* Needs C++11:
+   * launcher.cpp:*: warning: extended initializer lists only available with -std=c++11 or -std=gnu++11
+   */
+  for (int i = 0; i < screens_avail; ++i)
   {
-    /* this part needs C++11 */
-    if (i == screens_avail)
-    {
-      monitor_items[i] = ITEM_INIT;
-    }
-    else
-    {
-      std::string entry = "Screen " + itostr(i + 1);
-      monitor_items[i] = ITEM(entry.c_str());
-    }
+    monitor_entry[i] = "Screen " + itostr(i + 1);
+    monitor_items[i] = ITEM(monitor_entry[i].c_str());
   }
+  monitor_items[screens_avail] = ITEM_INIT;
 
   Fl_Color selection_color = fl_rgb_color(200, 18, 0);
   Fl::background(200, 18, 0);
@@ -294,13 +315,15 @@ int main(int argc, char **argv)
   win = new Fl_Window(400, 600, "SUPERHOT Launcher");
   win->callback(close_cb);
   {
-    Fl_Group *g = new Fl_Group(0, 0, 400, 600);
-#if EMBEDDED_RESSOURCES == 1
+    Fl_Group *g         = new Fl_Group(0, 0, 400, 600);
+#if EMBEDDED_RESOURCES == 1
     Fl_JPEG_Image *jpeg = new Fl_JPEG_Image(NULL, BGIMAGE);
 #else
-    Fl_JPEG_Image *jpeg = new Fl_JPEG_Image("launcher.jpg");
+    std::string bgimage = std::string(exedir) + "/launcher.jpg";
+    Fl_JPEG_Image *jpeg = new Fl_JPEG_Image(bgimage.c_str());
 #endif
-    Fl_Tiled_Image *wp = new Fl_Tiled_Image(jpeg);
+    Fl_Tiled_Image *wp  = new Fl_Tiled_Image(jpeg);
+
     g->image(wp);
     g->align(FL_ALIGN_INSIDE);
     {
@@ -358,11 +381,11 @@ int main(int argc, char **argv)
         o->labelcolor(FL_WHITE); }
 
       { Fl_Radio_Round_Button *o[2];
-        o[0] = lowend = new Fl_Radio_Round_Button(78, 370, 66, 26, " LOW");
+        o[0] = new Fl_Radio_Round_Button(78, 370, 66, 26, " LOW");
         o[0]->labelcolor(FL_WHITE);
         o[0]->clear_visible_focus();
         o[0]->callback(rb_callback, 0);
-        o[1] = highend = new Fl_Radio_Round_Button(160, 370, 70, 26, " HIGH");
+        o[1] = new Fl_Radio_Round_Button(160, 370, 70, 26, " HIGH");
         o[1]->labelcolor(FL_WHITE);
         o[1]->clear_visible_focus();
         o[1]->callback(rb_callback, 1);
@@ -385,8 +408,7 @@ int main(int argc, char **argv)
         o->clear_visible_focus();
         o->callback(launch_cb); }
 
-      { /* Twitter */
-        uri_box *o = new uri_box(119, 526, 43, 37, NULL);
+      { uri_box *o = new uri_box(119, 526, 43, 37, NULL);
 #if (DEBUG == 1)
         o->box(FL_UP_BOX);
         o->color(FL_BLUE);
@@ -394,10 +416,9 @@ int main(int argc, char **argv)
         o->box(FL_NO_BOX);
 #endif
         o->clear_visible_focus();
-        o->callback(open_uri_cb, (void *)uri1); }
+        o->callback(open_uri_cb, (void *)"https://twitter.com/superhotthegame"); }
 
-      { /* Facebook */
-        uri_box *o = new uri_box(188, 523, 26, 43, NULL);
+      { uri_box *o = new uri_box(188, 523, 26, 43, NULL);
 #if (DEBUG == 1)
         o->box(FL_UP_BOX);
         o->color(FL_BLUE);
@@ -405,10 +426,9 @@ int main(int argc, char **argv)
         o->box(FL_NO_BOX);
 #endif
         o->clear_visible_focus();
-        o->callback(open_uri_cb, (void *)uri2); }
+        o->callback(open_uri_cb, (void *)"https://www.facebook.com/superhotgame"); }
 
-      { /* eMail */
-        uri_box *o = new uri_box(240, 527, 43, 34, NULL);
+      { uri_box *o = new uri_box(240, 527, 43, 34, NULL);
 #if (DEBUG == 1)
         o->box(FL_UP_BOX);
         o->color(FL_BLUE);
@@ -416,7 +436,7 @@ int main(int argc, char **argv)
         o->box(FL_NO_BOX);
 #endif
         o->clear_visible_focus();
-        o->callback(open_uri_cb, (void *)uri3); }
+        o->callback(open_uri_cb, (void *)"mailto:people@superhotgame.com"); }
     }
     g->end();
   }
@@ -426,10 +446,11 @@ int main(int argc, char **argv)
   int pos_y = (Fl::h() - win->h()) / 2;
   win->position(pos_x, pos_y);
 
-#if EMBEDDED_RESSOURCES == 1
+#if EMBEDDED_RESOURCES == 1
   Fl_PNG_Image win_icon(NULL, ICON, (int) ICON_LEN);
 #else
-  Fl_PNG_Image win_icon("./SUPERHOT_Data/Resources/UnityPlayer.png");
+  std::string icon = std::string(exedir) + "/SUPERHOT_Data/Resources/UnityPlayer.png";
+  Fl_PNG_Image win_icon(icon.c_str());
 #endif
   Fl_Window::default_icon(&win_icon);
 
@@ -439,27 +460,31 @@ int main(int argc, char **argv)
 
   if (launch_game)
   {
-    val_res = resolution_selection->value();
-    val_lang = language_selection->value();
+    val_res       = resolution_selection->value();
+    val_lang      = language_selection->value();
+    val_screen    = screen_selection->value();
     is_fullscreen = (checkbutton_set) ? 0 : 1;
 
     /* write config file */
     std::ofstream of;
     of.open(conffile.c_str());
-    of << "resolution=" << itostr(val_res) << "\n"
-       << "screen=" << itostr(val_screen) << "\n"
+    of << "resolution=" << itostr(val_res)       << "\n"
+       << "screen="     << itostr(val_screen)    << "\n"
        << "fullscreen=" << itostr(is_fullscreen) << "\n"
-       << "language=" << itostr(val_lang) << "\n"
-       << "quality=" << itostr(val_quality) << "\n";
+       << "language="   << itostr(val_lang)      << "\n"
+       << "quality="    << itostr(val_quality)   << "\n";
     of.close();
 
-    std::string command = "./SUPERHOT." EXEEXT
-      " -adapter " + itostr(val_screen) +
+    std::string quality[] = { "LowEnd", "HighEnd" };
+    std::string l10n[] = { "EN", "PL", "FR", "DE", "RU", "PT", "IT", "CZ", "HU", "SP" };
+
+    std::string command = std::string(exedir) + "/SUPERHOT." EXEEXT
+      " -adapter "           + itostr(val_screen) +
       " -screen-fullscreen " + itostr(is_fullscreen) +
-      " -screen-height " + itostr(resolutions[val_res][1]) +
-      " -screen-width " + itostr(resolutions[val_res][0]) +
-      " -language " + l10n[val_lang] +
-      " -screen-quality " + quality[val_quality];
+      " -screen-height "     + itostr(resolutions[val_res][1]) +
+      " -screen-width "      + itostr(resolutions[val_res][0]) +
+      " -language "          + l10n[val_lang] +
+      " -screen-quality "    + quality[val_quality];
 
     Fl::check();
 
